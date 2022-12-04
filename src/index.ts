c89355c2c847870bff13f5e16c5d88ea00a3bb3f
cpl121 <cpldev121@gmail.com>
2022-12-04 13:59:34 +0100

    add gitignore and resolve day 4 of advant of code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as day1 from './day1';
 import * as day2 from './day2';
 import * as day3 from './day3';
+import * as day4 from './day4';
 
 console.log("--------------------- DAY 1 ---------------------");
 console.log("Result for day 1 part 1", day1.result1);
@@ -12,3 +13,7 @@ console.log("--------------------- DAY 3 ---------------------");
 console.log("Result for day 3 part 1", day3.result1);
 console.log("Result for day 3 part 2", day3.result2);
 console.log("--------------------- DAY 4 ---------------------");
+console.log("Result for day 4 part 1", day4.result1);
+console.log("Result for day 4 part 2", day4.result2);
+
+console.log("--------------------- DAY 5 ---------------------");
