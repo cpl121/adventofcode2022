1eedace7757a164f368ee4263ff67a9cbad652b5
cpl121 <cpldev121@gmail.com>
2022-12-06 11:45:19 +0100

    done day 6

---
+++ src/day6.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import { chunkArray } from '../utils/actions';
+
+const SEQUENCE_CHARACTERS = 4;
+const SEQUENCE_CHARACTERS_2 = 14;
+
+function getStartPacketMarker(data: string[], characters: number): number[] {
+    let result: number[] = []
+
+    for (let i = 0; i < data.length; i++) {
+        const element: string = data[i];
+        for (let j = 0; j < element.length; j++) {
+            const fourCharacters = element.slice(j, j + characters).split('')
+            
+            const hasSameCharacter: boolean = [...new Set(fourCharacters)].length === characters
+            if (!hasSameCharacter) {
+                continue
+            } else {
+                const index = element.indexOf(fourCharacters.join(''))
+                result.push(index + characters)
+                break;
+            }
+        }
+    }
+    return result
+}
+
+const data: string = fs.readFileSync('resources/day6data.txt', 'utf8')
+const arrayData: string[] = chunkArray(data)
+
+// Part 1
+export const result1 = getStartPacketMarker(arrayData, SEQUENCE_CHARACTERS)
+
+// Part 2
+export const result2 = getStartPacketMarker(arrayData, SEQUENCE_CHARACTERS_2)
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as day2 from './day2';
 import * as day3 from './day3';
 import * as day4 from './day4';
 import * as day5 from './day5';
+import * as day6 from './day6';
 
 console.log("--------------------- DAY 1 ---------------------");
 console.log("Result for day 1 part 1", day1.result1);
@@ -20,3 +21,6 @@ console.log("--------------------- DAY 5 ---------------------");
 console.log("Result for day 5 part 1", day5.result1);
 console.log("Result for day 5 part 2", day5.result2);
 console.log("--------------------- DAY 6 ---------------------");
+console.log("Result for day 6 part 1", day6.result1);
+console.log("Result for day 6 part 2", day6.result2);
+console.log("--------------------- DAY 7 ---------------------");
